7c5d45de5e9955a17cf356fe7af9382e3d5b458c
amelasz <amela.samardzic@student.htldornbirn.at>
2022-04-28 08:42:31 +0200

    01_Variablen_Operationen.html fertig

---
+++ JavaScript/01_Variablen_Operationen.js
@@ -0,0 +1,48 @@
+console.log("Hallo, willkommen.");
+console.log("Schön dich zu sehen");
+
+// Variablen
+
+let a = 7;
+let b = 8;
+let c = a + b;
+let d = (a + b) / c;
+let e = c - a + b;
+let f = a + b + c * 3;
+let g = (a + b + c) / 4;
+
+console.log("Der Wert von a beträgt:", a, "\n" + "Der Wert von b beträgt:", b, "\n" + "Der Wert von c beträgt:", c)
+
+// Berechne folgendes und gib das Ergebnis über console.log aus
+
+// a + b = c
+console.log("Das Ergebnis von a + b lautet ", c);
+console.log("a + b =", c);
+
+// (a + b) / c
+console.log("Das Ergebnis von (a + b) / c lautet ", d);
+console.log("(a + b) / c =", d)
+
+// c - a + b
+console.log("Das Ergebnis von c - a + b lautet ", e);
+console.log("c - a + b =", e);
+
+// a + b + c * 3
+console.log("Das Ergebnis von a + b + c * 3 lautet ", f);
+console.log("a + b + c * 3 =", f);
+
+// (a + b + c) / 4
+console.log("Das Ergebnis von (a + b + c) / 4 lautet ", g);
+console.log("(a + b + c) / 4 =", );
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+// b = a + 3
+// c = c -3
+// Gib die neuen Werte der Variablen aus
+
+a = 3;
+b = a + 3
+c = c - 3
+
+console.log("Die neuen Werte für die Variablen a, b und c sind", a + ",", b, "und", c, ".")
