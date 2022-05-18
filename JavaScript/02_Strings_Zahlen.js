775868261feb89b93639a0108193d704f65ffe21
amelasz <amela.samardzic@student.htldornbirn.at>
2022-05-18 10:52:46 +0200

    02 Strings & Zahlen fertig

---
+++ JavaScript/02_Strings_Zahlen.js
@@ -0,0 +1,36 @@
+let a = 5;
+let b = 6;
+
+let c = 7.4;
+let d = 7.5;
+
+let result = c + d;
+console.log(result);
+
+//Texte = Strings
+
+let namee = "Amela";
+console.log(namee);
+
+console.log("Die Summe von " + a + " und " + b + " ist: " + result);
+
+
+// Aufgabe 1
+// Berechne das Ergebnis von 17.4 / 3.9. 
+// Gib folgenden Satz aus: The result of 17.4 divided by 3.9 is 4.461538461538462
+
+let e = 17.4;
+let f = 3.9;
+result = e/f;
+
+console.log("The result of " + e + " divided by " + f + " is:", result);
+
+// Aufgabe 2
+// Lege eine Variable firstname und eine Varialbe lastname an. 
+// Weise den Variablen Deinen Namen zu. Gib folgendes aus: 
+// "Ich heiße VORNAME NACHNAME"
+
+let firstname = "Amela";
+let lastname = "Samardzic";
+
+console.log("Ich heiße " + firstname + " " + lastname);
