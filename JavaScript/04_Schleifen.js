8ee1b2fc67e6782f99b80f6c5dc8989ba2c00df0
amelasz <amela.samardzic@student.htldornbirn.at>
2022-05-19 09:10:49 +0200

    04 Schleifen fertig

---
+++ JavaScript/04_Schleifen.js
@@ -0,0 +1,66 @@
+console.log("Hallo Test");
+
+let a = 7;
+let b = 4.3;
+let name = "Amela";
+
+console.log(name , a , b);
+
+for (let count = 0; count<10; count++){
+    console.log(count + " " + "Amela")
+}
+
+// Zähle von 7 bis 45
+console.log("Zähle von 7 bis 45");
+
+for(let number = 7; number <= 45; number++)
+{
+    console.log(number);
+}
+
+// Zähle von 9 bis 99
+console.log("Zähle von 9 bis 99");
+
+for(let number = 9; number <= 99; number++)
+{
+    console.log(number);
+}
+
+// Zähle rückwärts von 10 bis 0
+console.log("Zähle rückwärts von 10 bis 0");
+
+for(let reverseCount = 10; reverseCount  >= 0; reverseCount --)
+{
+    console.log(reverseCount);
+}
+
+// Zähle rückwärts von 1234 bis 1207
+console.log("Zähle rückwärts von 1234 bis 1207");
+
+for(let reverseCount2 = 1234; reverseCount2  >= 1207; reverseCount2 --)
+{
+    console.log(reverseCount2);
+}
+
+// Gib deinen Namen genau 8 mal aus
+console.log("Gib deinen Namen genau 8 mal aus");
+
+for(let namecount = 1; namecount <= 8; namecount++)
+{
+    console.log(name);
+}
+
+// Zähle von 0 auf 10, dann gib deinen Namen aus, dann zähle von 10 auf 0
+console.log("Zähle von 0 auf 10, dann gib deinen Namen aus, dann zähle von 10 auf 0");
+
+for(count = 0; count <= 10; count++)
+{
+    console.log(count);
+}
+
+console.log(name);
+
+for(count = 10; count >= 0; count--)
+{
+    console.log(count);
+}
